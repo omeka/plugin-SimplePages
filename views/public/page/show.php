9781b2ef5fd669f02f23e3d4112df755ee690705
Jeremy Boggs <jeremy@clioweb.org>
2008-11-03 20:36:13 +0000

    Adds template tags to the public page view.

--- views/public/page/show.php
+++ views/public/page/show.php
@@ -1,1 +1,8 @@
-<?php echo eval('?>' . $page->text); ?>
+<?php head(array('title' => $page->title)); ?>
+
+<div id="primary">
+	<h1><?php echo $page->title; ?></h1>
+	<?php echo eval('?>' . $page->text); ?>
+</div>
+
+<?php echo foot(); ?>
