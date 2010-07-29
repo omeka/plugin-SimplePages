d62073861a60d9cfd7e57d2161cdfb41d754ff8a
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-07-29 19:25:18 +0000

    Fixes invalid index notices on browse view that were causing test failures.

--- views/admin/index/browse.php
+++ views/admin/index/browse.php
@@ -5,10 +5,10 @@ head($head);
 <h1><?php echo $head['title']; ?></h1>
 
 <ul id="section-nav" class="navigation">
-    <li class="<?php if ($_GET['view'] != 'hierarchy') {echo 'current';} ?>">
+    <li class="<?php if (isset($_GET['view']) &&  $_GET['view'] != 'hierarchy') {echo 'current';} ?>">
         <a href="<?php echo html_escape(uri('simple-pages/index/browse?view=list')); ?>">List View</a>
     </li>
-    <li class="<?php if ($_GET['view'] == 'hierarchy') {echo 'current';} ?>">
+    <li class="<?php if (isset($_GET['view']) && $_GET['view'] == 'hierarchy') {echo 'current';} ?>">
         <a href="<?php echo html_escape(uri('simple-pages/index/browse?view=hierarchy')); ?>">Hierarchy View</a>
     </li>
 </ul>
@@ -19,7 +19,7 @@ head($head);
 <?php if (!has_simple_pages_for_loop()): ?>
     <p>There are no pages. Why not <a href="<?php echo html_escape(uri('simple-pages/index/add')); ?>">add one</a>?</p>
 <?php else: ?>
-    <?php if ($_GET['view'] == 'hierarchy'): ?>
+    <?php if (isset($_GET['view']) && $_GET['view'] == 'hierarchy'): ?>
         <?php echo $this->partial('index/browse-hierarchy.php', array('simplePages' => get_simple_pages_for_loop())); ?>
     <?php else: ?>
         <?php echo $this->partial('index/browse-list.php', array('simplePages' => get_simple_pages_for_loop())); ?>
