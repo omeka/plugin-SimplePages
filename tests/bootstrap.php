eeee4cb8e504afb1dff0093749b573fd32bb5f02
John Flatness <john@zerocrates.org>
2012-01-11 16:22:29 -0500

    Tweak test bootstrap for running all tests with Ant.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @copyright Roy Rosenzweig Center for History and New Media, 2007-2011
- * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @package SimplePages
- */
 
-require_once '../../../application/tests/bootstrap.php';
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/application/tests/bootstrap.php';
 require_once 'SimplePages_Test_AppTestCase.php';
-
