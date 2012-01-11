e2cb250c875faedc92c38b6064d786f574774566
John Flatness <john@zerocrates.org>
2012-01-11 11:57:14 -0500

    Fix tests for PHPUnit 3.6.

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright Roy Rosenzweig Center for History and New Media, 2007-2011
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package SimplePages
+ */
+
+require_once '../../../application/tests/bootstrap.php';
+require_once 'SimplePages_Test_AppTestCase.php';
+
