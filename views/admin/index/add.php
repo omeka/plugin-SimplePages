6ab44a9f50e852493059db9ca4ccc51accafc76a
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-10-01 16:11:51 -0400

    Updates add and edit views, and form to use Omeka_Form_Admin

--- views/admin/index/add.php
+++ views/admin/index/add.php
@@ -1,18 +1,42 @@
 <?php
+
+queue_js_file('tiny_mce/tiny_mce');
 $head = array('bodyclass' => 'simple-pages primary', 
               'title' => html_escape(__('Simple Pages | Add Page')));
 echo head($head);
 ?>
+
+<script type="text/javascript">
+jQuery(window).load(function() {
+    // Initialize and configure TinyMCE.
+    tinyMCE.init({
+        // Assign TinyMCE a textarea:
+        mode : 'exact',
+        elements: '<?php if (simple_page('use_tiny_mce')) echo 'simple-pages-text'; ?>',
+        // Add plugins:
+        plugins: 'media,paste,inlinepopups',
+        // Configure theme:
+        theme: 'advanced',
+        theme_advanced_toolbar_location: 'top',
+        theme_advanced_toolbar_align: 'left',
+        theme_advanced_buttons3_add : 'pastetext,pasteword,selectall',
+        // Allow object embed. Used by media plugin
+        // See http://www.tinymce.com/forum/viewtopic.php?id=24539
+        media_strict: false,
+        // General configuration:
+        convert_urls: false,
+    });
+    // Add or remove TinyMCE control.
+    jQuery('#simple-pages-use-tiny-mce').click(function() {
+        if (jQuery(this).is(':checked')) {
+            tinyMCE.execCommand('mceAddControl', true, 'simple-pages-text');
+        } else {
+            tinyMCE.execCommand('mceRemoveControl', true, 'simple-pages-text');
+        }
+    });
+});
+</script>
 <?php echo flash(); ?>
-<form method="post">
-    <div class="seven columns alpha">
         <?php include 'form.php'; ?>
-    </div>
-    <div id="save" class="three columns omega panel">
-        <?php echo $this->formSubmit('simple-pages-add-submit', 
-                                     __('Add Page'), 
-                                     array('id'    => 'simple-pages-add-submit', 
-                                           'class' => 'submit big green button')); ?>
-    </div>
-</form>
+        <?php echo new SimplePagesAdminForm(); ?>
 <?php echo foot(); ?>
--- views/admin/index/edit.php
+++ views/admin/index/edit.php
@@ -1,25 +1,50 @@
 <?php
+
+
+queue_js_file('tiny_mce/tiny_mce');
 $head = array('bodyclass' => 'simple-pages primary', 
               'title' => html_escape(__('Simple Pages | Edit "%s"', simple_page('title'))));
 echo head($head);
 ?>
+
+
+<script type="text/javascript">
+jQuery(window).load(function() {
+    // Initialize and configure TinyMCE.
+    tinyMCE.init({
+        // Assign TinyMCE a textarea:
+        mode : 'exact',
+        elements: '<?php if (simple_page('use_tiny_mce')) echo 'simple-pages-text'; ?>',
+        // Add plugins:
+        plugins: 'media,paste,inlinepopups',
+        // Configure theme:
+        theme: 'advanced',
+        theme_advanced_toolbar_location: 'top',
+        theme_advanced_toolbar_align: 'left',
+        theme_advanced_buttons3_add : 'pastetext,pasteword,selectall',
+        // Allow object embed. Used by media plugin
+        // See http://www.tinymce.com/forum/viewtopic.php?id=24539
+        media_strict: false,
+        // General configuration:
+        convert_urls: false,
+    });
+    // Add or remove TinyMCE control.
+    jQuery('#simple-pages-use-tiny-mce').click(function() {
+        if (jQuery(this).is(':checked')) {
+            tinyMCE.execCommand('mceAddControl', true, 'simple-pages-text');
+        } else {
+            tinyMCE.execCommand('mceRemoveControl', true, 'simple-pages-text');
+        }
+    });
+});
+</script>
+
 <?php echo flash(); ?>
 <p><?php echo __('This page was created by <strong>%1$s</strong> on %2$s, and last modified by <strong>%3$s</strong> on %4$s.',
     html_escape(get_current_simple_page()->getCreatedByUser()->username),
     html_escape(format_date(simple_page('inserted'), Zend_Date::DATETIME_MEDIUM)),
     html_escape(get_current_simple_page()->getModifiedByUser()->username), 
     html_escape(format_date(simple_page('updated'), Zend_Date::DATETIME_MEDIUM))); ?></p>
-<form method="post">
-    <div class="seven columns alpha">
-        <?php include 'form.php'; ?>
-    </div>
-
-    <div class="three columns omega panel" id="save">
-        <?php echo $this->formSubmit('simple-pages-edit-submit',
-            __('Save Page'), 
-            array('id'    => 'simple-pages-edit-submit', 
-            'class' => 'submit big green button')); ?>
-        <a href="<?php echo url(array('action' => 'delete-confirm')); ?>" class="big red button delete-confirm"><?php echo __('Delete'); ?></a>
-    </div>
-</form>
+<?php include 'form.php'; ?>
+<?php echo new SimplePagesAdminForm(array('record' => get_current_record('simple_pages_page'))); ?>
 <?php echo foot(); ?>
