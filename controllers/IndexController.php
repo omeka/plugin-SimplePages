537bf9e449437b359a5a6e5e0ea4b8ab6a0ea836
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-11-10 12:56:35 -0500

    Updates to new options for admin form

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -50,7 +50,7 @@ class SimplePages_IndexController extends Omeka_Controller_AbstractActionControl
     
     protected function _getForm($page = null)
     { 
-        $formOptions = array('recordType' => 'simple_pages_page');
+        $formOptions = array('type' => 'simple_pages_page');
         if($page && $page->exists()) {
             $formOptions['record'] = $page;
         }
