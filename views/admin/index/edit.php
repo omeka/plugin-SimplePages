125da6fe4b74e000bd50889ed587e75237b1802a
John Flatness <john@zerocrates.org>
2018-01-25 14:59:09 -0500

    Use tinyMCE 4

--- views/admin/index/edit.php
+++ views/admin/index/edit.php
@@ -1,36 +1,21 @@
 <?php
-queue_js_file('vendor/tiny_mce/tiny_mce');
+queue_js_file('vendor/tinymce/tinymce.min');
 $head = array('bodyclass' => 'simple-pages primary', 
               'title' => __('Simple Pages | Edit "%s"', metadata('simple_pages_page', 'title')));
 echo head($head);
 ?>
 
 <script type="text/javascript">
-jQuery(window).load(function() {
-    // Initialize and configure TinyMCE.
-    tinyMCE.init({
-        // Assign TinyMCE a textarea:
-        mode : 'exact',
-        elements: '<?php if ($simple_pages_page->use_tiny_mce) echo 'simple-pages-text'; ?>',
-        // Add plugins:
-        plugins: 'media,paste,inlinepopups',
-        // Configure theme:
-        theme: 'advanced',
-        theme_advanced_toolbar_location: 'top',
-        theme_advanced_toolbar_align: 'left',
-        theme_advanced_buttons3_add : 'pastetext,pasteword,selectall',
-        // Allow object embed. Used by media plugin
-        // See http://www.tinymce.com/forum/viewtopic.php?id=24539
-        media_strict: false,
-        // General configuration:
-        convert_urls: false,
+jQuery(document).ready(function() {
+    Omeka.wysiwyg({
+        selector: <?php echo json_encode($simple_pages_page->use_tiny_mce ? '#simple-pages-text' : false); ?>,
     });
     // Add or remove TinyMCE control.
     jQuery('#simple-pages-use-tiny-mce').click(function() {
         if (jQuery(this).is(':checked')) {
-            tinyMCE.execCommand('mceAddControl', true, 'simple-pages-text');
+            tinyMCE.EditorManager.execCommand('mceAddEditor', true, 'simple-pages-text');
         } else {
-            tinyMCE.execCommand('mceRemoveControl', true, 'simple-pages-text');
+            tinyMCE.EditorManager.execCommand('mceRemoveEditor', true, 'simple-pages-text');
         }
     });
 });
