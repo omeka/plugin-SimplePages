6eb29577ea3c0215eafbc2ffcf3abc3f297fb1de
John Flatness <john@zerocrates.org>
2018-04-27 15:30:17 -0400

    Restore some more tinymce3 features
    Also, finally factor out common wysiwyg JS between edit and add.

--- views/admin/index/add.php
+++ views/admin/index/add.php
@@ -1,27 +1,12 @@
 <?php
 
 queue_js_file('vendor/tinymce/tinymce.min');
+queue_js_file('simple-pages-wysiwyg');
 $head = array('bodyclass' => 'simple-pages primary', 
               'title' => html_escape(__('Simple Pages | Add Page')));
 echo head($head);
 ?>
 
-<script type="text/javascript">
-jQuery(document).ready(function() {
-    Omeka.wysiwyg({
-        selector: <?php echo json_encode($simple_pages_page->use_tiny_mce ? '#simple-pages-text' : false); ?>,
-        menubar: 'edit insert view format'
-    });
-    // Add or remove TinyMCE control.
-    jQuery('#simple-pages-use-tiny-mce').click(function() {
-        if (jQuery(this).is(':checked')) {
-            tinyMCE.EditorManager.execCommand('mceAddEditor', true, 'simple-pages-text');
-        } else {
-            tinyMCE.EditorManager.execCommand('mceRemoveEditor', true, 'simple-pages-text');
-        }
-    });
-});
-</script>
 <?php echo flash(); ?>
 <?php echo $form; ?>
 <?php echo foot(); ?>
--- views/admin/index/edit.php
+++ views/admin/index/edit.php
@@ -1,26 +1,10 @@
 <?php
 queue_js_file('vendor/tinymce/tinymce.min');
+queue_js_file('simple-pages-wysiwyg');
 $head = array('bodyclass' => 'simple-pages primary', 
               'title' => __('Simple Pages | Edit "%s"', metadata('simple_pages_page', 'title')));
 echo head($head);
 ?>
-
-<script type="text/javascript">
-jQuery(document).ready(function() {
-    Omeka.wysiwyg({
-        selector: <?php echo json_encode($simple_pages_page->use_tiny_mce ? '#simple-pages-text' : false); ?>,
-        menubar: 'edit insert view format'
-    });
-    // Add or remove TinyMCE control.
-    jQuery('#simple-pages-use-tiny-mce').click(function() {
-        if (jQuery(this).is(':checked')) {
-            tinyMCE.EditorManager.execCommand('mceAddEditor', true, 'simple-pages-text');
-        } else {
-            tinyMCE.EditorManager.execCommand('mceRemoveEditor', true, 'simple-pages-text');
-        }
-    });
-});
-</script>
 
 <?php echo flash(); ?>
 <p><?php echo __('This page was created by <strong>%1$s</strong> on %2$s, and last modified by <strong>%3$s</strong> on %4$s.',
