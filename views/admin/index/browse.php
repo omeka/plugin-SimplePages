c71d436cf19142e95e0ba45ae2fc45920d0c281e
John Flatness <john@zerocrates.org>
2021-10-27 13:28:31 -0400

    Fix notices on admin browse

--- views/admin/index/browse.php
+++ views/admin/index/browse.php
@@ -3,16 +3,17 @@ queue_css_file('simple-pages');
 $head = array('bodyclass' => 'simple-pages browse',
               'title' => html_escape(__('Simple Pages | Browse')),
               'content_class' => 'horizontal-nav');
+$hierarchy = false;
 if (isset($_GET['view'])) {
-	$active = $_GET['view'];
+	$hierarchy = $_GET['view'] == 'hierarchy';
 }
 echo head($head);
 ?>
 <ul id="section-nav" class="navigation">
-    <li class="<?php if ($active != 'hierarchy') {echo 'current';} ?>">
+    <li class="<?php if (!$hierarchy) {echo 'current';} ?>">
         <a href="<?php echo html_escape(url('simple-pages/index/browse?view=list')); ?>"><?php echo __('List View'); ?></a>
     </li>
-    <li class="<?php if ($active == 'hierarchy') {echo 'current';} ?>">
+    <li class="<?php if ($hierarchy) {echo 'current';} ?>">
         <a href="<?php echo html_escape(url('simple-pages/index/browse?view=hierarchy')); ?>"><?php echo __('Hierarchy View'); ?></a>
     </li>
 </ul>
@@ -22,7 +23,7 @@ echo head($head);
 <?php if (!has_loop_records('simple_pages_page')): ?>
     <p><?php echo __('There are no pages.'); ?> <a href="<?php echo html_escape(url('simple-pages/index/add')); ?>"><?php echo __('Add a page.'); ?></a></p>
 <?php else: ?>
-    <?php if (isset($_GET['view']) && $_GET['view'] == 'hierarchy'): ?>
+    <?php if ($hierarchy): ?>
         <?php echo $this->partial('index/browse-hierarchy.php', array('simplePages' => $simple_pages_pages)); ?>
     <?php else: ?>
         <?php echo $this->partial('index/browse-list.php', array('simplePages' => $simple_pages_pages)); ?>
