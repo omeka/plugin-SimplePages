1c67f62c99e4b1bb15e739b82f1bd57f2d9db18d
wojtekarpinski <wa.karpinski@uw.edu.pl>
2022-11-10 15:37:51 +0100

    wysiwyg 2

--- views/admin/javascripts/simple-exhibits-wysiwyg.js
+++ views/admin/javascripts/simple-exhibits-wysiwyg.js
@@ -11,6 +11,19 @@ jQuery(document).ready(function() {
         plugins: 'lists link code paste media autoresize image table charmap hr',
         browser_spellcheck: true
     });
+    var selector;
+    if (jQuery('#simple-exhibits-use-tiny-mce').is(':checked')) {
+        selector = '#simple-exhibits-text';
+    } else {
+        selector = false;
+    }
+    Omeka.wysiwyg({
+        selector: selector,
+        menubar: 'edit view insert format table',
+        plugins: 'lists link code paste media autoresize image table charmap hr',
+        browser_spellcheck: true
+    });
+
     // Add or remove TinyMCE control.
     jQuery('#simple-exhibits-use-tiny-mce').click(function() {
         if (jQuery(this).is(':checked')) {
