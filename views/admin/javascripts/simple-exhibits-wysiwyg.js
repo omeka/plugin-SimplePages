e9b7cdd8cc6767519c4b3f311459981641ab967c
wojtekarpinski <wa.karpinski@uw.edu.pl>
2022-11-10 15:42:30 +0100

    wysiwyg 3

--- views/admin/javascripts/simple-exhibits-wysiwyg.js
+++ views/admin/javascripts/simple-exhibits-wysiwyg.js
@@ -1,18 +1,6 @@
 jQuery(document).ready(function() {
     var selector;
     if (jQuery('#simple-exhibits-use-tiny-mce').is(':checked')) {
-        selector = '#simple-exhibits-content';
-    } else {
-        selector = false;
-    }
-    Omeka.wysiwyg({
-        selector: selector,
-        menubar: 'edit view insert format table',
-        plugins: 'lists link code paste media autoresize image table charmap hr',
-        browser_spellcheck: true
-    });
-    var selector;
-    if (jQuery('#simple-exhibits-use-tiny-mce').is(':checked')) {
         selector = '#simple-exhibits-text';
     } else {
         selector = false;
@@ -27,11 +15,35 @@ jQuery(document).ready(function() {
     // Add or remove TinyMCE control.
     jQuery('#simple-exhibits-use-tiny-mce').click(function() {
         if (jQuery(this).is(':checked')) {
-            tinyMCE.EditorManager.execCommand('mceAddEditor', true, 'simple-exhibits-content');
             tinyMCE.EditorManager.execCommand('mceAddEditor', true, 'simple-exhibits-text');
         } else {
-            tinyMCE.EditorManager.execCommand('mceRemoveEditor', true, 'simple-exhibits-content');
-            tinyMCE.EditorManager.execCommand('mceAddEditor', true, 'simple-exhibits-text');
+            tinyMCE.EditorManager.execCommand('mceRemoveEditor', true, 'simple-exhibits-text');
         }
     });
+
+    var selector;
+    if (jQuery('#simple-exhibits-use-tiny-mce').is(':checked')) {
+        selector = '#simple-exhibits-content';
+    } else {
+        selector = false;
+    }
+    Omeka.wysiwyg({
+        selector: selector,
+        menubar: 'edit view insert format table',
+        plugins: 'lists link code paste media autoresize image table charmap hr',
+        browser_spellcheck: true
+    });
+
+    // Add or remove TinyMCE control.
+    jQuery('#simple-exhibits-use-tiny-mce').click(function() {
+        if (jQuery(this).is(':checked')) {
+            tinyMCE.EditorManager.execCommand('mceAddEditor', true, 'simple-exhibits-content');
+        } else {
+            tinyMCE.EditorManager.execCommand('mceRemoveEditor', true, 'simple-exhibits-content');
+        }
+    });
+
+
+
 });
+
